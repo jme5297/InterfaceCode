057a1c8aad6e4ebd5bca980b9fb71b9f59b2bf1a
amc6630 <amc6630@psu.edu>
2018-03-13 18:39:42 -0400

    pwm_disabler updates

--- pwm/pwm_disabler.c
+++ pwm/pwm_disabler.c
@@ -5,13 +5,20 @@
 #include <stdlib.h>
 #include <prussdrv.h>
 #include <pruss_intc_mapping.h>
+#include <unistd.h>
 
 #define PRU_NUM   0
 
 int main (void)
 {
-   tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
-   prussdrv_pru_disable(PRU_NUM);
-   prussdrv_exit ();
-   return EXIT_SUCCESS;
+  tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+  prussdrv_init ();
+  prussdrv_open (PRU_EVTOUT_0);
+  prussdrv_pruintc_init(&pruss_intc_initdata);
+  prussdrv_pru_write_memory(PRUSS0_PRU0_DATARAM, 0, 0, 4);
+  prussdrv_exec_program (PRU_NUM, "./pwm_final.bin");
+  usleep(100000);
+  prussdrv_pru_disable(PRU_NUM);
+  prussdrv_exit ();
+  return 0;
  }
