d0c1e953dc0bfa80cb1c8aaa1adb2cdb13b93c69
amc6630 <amc6630@psu.edu>
2018-03-15 18:46:09 -0400

    code for pru #1

---
+++ pwm/pwm_disabler_pru2.c
@@ -0,0 +1,21 @@
+// Program to disable execution of PRUSS code (.p)
+// Written by amc6630 on 3/11/2018
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <prussdrv.h>
+#include <pruss_intc_mapping.h>
+#include <unistd.h>
+
+#define PRU_NUM   1
+
+int main (void)
+{
+  tpruss_intc_initdata pruss_intc_initdata = PRUSS_INTC_INITDATA;
+  prussdrv_init ();
+  prussdrv_open (PRU_EVTOUT_0);
+  prussdrv_pruintc_init(&pruss_intc_initdata);
+  prussdrv_exec_program (PRU_NUM, "./pwm_final_pru2.bin");
+  unsigned int mode = 0;
+  prussdrv_pru_write_memory(PRUSS0_PRU1_DATARAM, 3, &mode, 4);
+ }
