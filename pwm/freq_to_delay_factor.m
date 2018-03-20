64537ef368edbc8c8bba99561b2f85dfc7e3b973
Jason <jme5297@psu.edu>
2018-03-19 23:57:52 -0400

    Added converter

---
+++ pwm/freq_to_delay_factor.m
@@ -0,0 +1,12 @@
+clear
+clc
+
+% Calculate frequency
+Ns = 100; %Samples per period
+D = 624; %Delay factor
+f = ( (5e-9)*2*(D+1)*Ns )^-1
+
+% Calculate delay factor
+Ns = 100; %Samples per second
+f = 1600; % frequency
+D = ( (5e-9)*2*f*Ns )^-1 - 1
