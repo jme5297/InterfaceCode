192c53a85f7a048c8c5dea2f609c50421a087fbf
amc6630 <amc6630@psu.edu>
2018-03-27 20:41:20 -0400

    GPS Code Update

--- gps.cpp
+++ gps.cpp
@@ -2,27 +2,17 @@
 #include <unistd.h>			//Used for UART
 #include <fcntl.h>			//Used for UART
 #include <termios.h>		//Used for UART
-
+#include <stdlib.h>
+#include <string.h>
+unsigned char rx_buffer[80];
+void process();
+void data_GR(char * buffer, int loc);
+double lat, lon, cog;
 int main(){
 
-  //-------------------------
-	//----- SETUP USART 0 -----
-	//-------------------------
-	//At bootup, pins 8 and 10 are already set to UART0_TXD, UART0_RXD (ie the alt0 function) respectively
 	int uart0_filestream = -1;
 
-	//OPEN THE UART
-	//The flags (defined in fcntl.h):
-	//	Access modes (use 1 of these):
-	//		O_RDONLY - Open for reading only.
-	//		O_RDWR - Open for reading and writing.
-	//		O_WRONLY - Open for writing only.
-	//
-	//	O_NDELAY / O_NONBLOCK (same function) - Enables nonblocking mode. When set read requests on the file can return immediately with a failure status
-	//											if there is no input immediately available (instead of blocking). Likewise, write requests can also return
-	//											immediately with a failure status if the output can't be written immediately.
-	//
-	//	O_NOCTTY - When set and path identifies a terminal device, open() shall not cause the terminal device to become the controlling terminal for the process.
+
 	uart0_filestream = open("/dev/ttyO4", O_RDWR | O_NOCTTY | O_NDELAY);		//Open in non blocking read/write mode
 	if (uart0_filestream == -1)
 	{
@@ -30,16 +20,6 @@ int main(){
 		printf("Error - Unable to open UART.  Ensure it is not in use by another application\n");
 	}
 
-	//CONFIGURE THE UART
-	//The flags (defined in /usr/include/termios.h - see http://pubs.opengroup.org/onlinepubs/007908799/xsh/termios.h.html):
-	//	Baud rate:- B1200, B2400, B4800, B9600, B19200, B38400, B57600, B115200, B230400, B460800, B500000, B576000, B921600, B1000000, B1152000, B1500000, B2000000, B2500000, B3000000, B3500000, B4000000
-	//	CSIZE:- CS5, CS6, CS7, CS8
-	//	CLOCAL - Ignore modem status lines
-	//	CREAD - Enable receiver
-	//	IGNPAR = Ignore characters with parity errors
-	//	ICRNL - Map CR to NL on input (Use for ASCII comms where you want to auto correct end of line characters - don't use for bianry comms!)
-	//	PARENB - Parity enable
-	//	PARODD - Odd parity (else even)
 	struct termios options;
 	tcgetattr(uart0_filestream, &options);
 	options.c_cflag = B9600 | CS8 | CLOCAL | CREAD;		//<Set baud rate
@@ -49,34 +29,57 @@ int main(){
 	tcflush(uart0_filestream, TCIFLUSH);
 	tcsetattr(uart0_filestream, TCSANOW, &options);
 
-
-  //----- CHECK FOR ANY RX BYTES -----
 	if (uart0_filestream != -1)
 	{
-
     while(1){
-  		// Read up to 255 characters from the port if they are there
-  		unsigned char rx_buffer[256];
-  		int rx_length = read(uart0_filestream, (void*)rx_buffer, 255);		//Filestream, buffer to store in, number of bytes to read (max)
-  		if (rx_length < 0)
-  		{
-  			//An error occured (will occur if there are no bytes)
-  		}
-  		else if (rx_length == 0)
-  		{
-  			//No data waiting
-  		}
-  		else
-  		{
-  			//Bytes received
-  			rx_buffer[rx_length] = '\0';
-  			printf("%i bytes read : %s\n", rx_length, rx_buffer);
+
+			char temp = uart0_filestream;
+
+			int i = 0;
+			if(temp != '\n' && i < 80){
+				rx_buffer[i] = temp;
+				i++;
+			}
+			else{
+				rx_buffer[i] = '\0';
+				i=0;
+				process();
+			}
   		}
     }
-	}
+
 
   //----- CLOSE THE UART -----
-	close(uart0_filestream);
+//	close(uart0_filestream);
 
   return 0;
+}
+void process(){
+char field[20];
+data_GR(field, 0);
+if (strcmp(field, "$GPRMC") == 0){
+	data_GR(field, 3);
+		lat = strtod(field,NULL);
+	data_GR(field, 5);
+		lon = strtod(field,NULL);
+	data_GR(field, 8);
+		cog =  strtod(field,NULL);
+}
+}
+void data_GR(char * buffer, int loc){
+int sentencePos =0;
+int fieldPos = 0;
+int commacount =0;
+while (sentencePos < 80){
+if(rx_buffer[sentencePos] == ','){
+	commacount ++;
+	sentencePos ++;
+}
+if(commacount == loc){
+buffer[fieldPos] = rx_buffer[sentencePos];
+fieldPos++;
+}
+sentencePos++;
+}
+buffer[fieldPos] = '\0';
 }
