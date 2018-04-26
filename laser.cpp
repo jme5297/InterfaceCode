3b1878e435c3535540d2922db66bed0f1afc1c62
root <root@beaglebone.localdomain>
2018-04-26 14:24:26 +0000

    Updates

--- laser.cpp
+++ laser.cpp
@@ -10,44 +10,43 @@
 
 using namespace std;
 
-inline void Detect()
-{
-    int digit = -1;
-    ifstream laser;
-    laser.open("/sys/class/gpio/gpio26/value");
-    
-    if (laser.is_open())
-    {
-        while (laser.good() )
-        {
-            laser >> digit;
-        }
-    }
-    
-   // laser.close();
-    
-    else 
-    {
-        cout << "Unable to read the data\n";
-        return;
-    }
-    
-    if (digit == 0) //obstacle detected
-    {
-        cout<<"Obstacle detected"<<endl;
-    }
-    else if (digit == 1) //no obstacle detect
-    {
-        cout<<" No obstacle detected"<<endl;
-    }
-}
-
-int main()
+int main(int argc, char** argv)
 {
     while(1)
     {
-        usleep(1);
-        Detect();
+        usleep(100000);
+
+	    int digit = -1;
+	    ifstream laser;
+	std::string loc = "/sys/class/gpio/gpio";
+	loc.append(argv[1]);
+	loc.append("/value");
+	    laser.open(loc);
+
+	    if (laser.is_open())
+	    {
+	        while (laser.good() )
+	        {
+	            laser >> digit;
+	        }
+	    }
+
+	   // laser.close();
+	    else 
+	    {
+	        cout << "Unable to read the data\n";
+	        continue;
+	    }
+	    
+	    if (digit == 0) //obstacle detected
+	    {
+	        cout<<"Obstacle detected"<<endl;
+	    }
+	    else if (digit == 1) //no obstacle detect
+		{
+       	 	cout<<" No obstacle detected"<<endl;
+	    	}
+
     }
     
     return 0;
